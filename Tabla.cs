6957417f571c57871f5ab9df6ea4ae910ed62556
Lperez14 <79065428+Lperez14@users.noreply.github.com>
2021-03-04 23:26:47 -0400

    Add files via upload

---
+++ Tabla.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio9
+{
+    class Tabla
+    {
+
+        public void cargar(int numero)
+        {
+            for (int o = 1; o < 12; o++)
+            {
+                Console.WriteLine(o + " X " + numero + " = " + numero * o); ;
+            }
+        }
+    }
+}
---
+++ valores.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio10
+{
+    class valores
+    {
+        public string nombre{ get; set; }
+        public int edad { get; set; }
+
+        public valores(string nombre, int edad)
+        {
+            this.nombre = nombre;
+            this.edad = edad;
+        }
+        public void cargar()
+        {
+
+            Console.WriteLine("Nombre de la persona:" + nombre);
+            Console.WriteLine("Edad de la persona: " + edad);
+
+            if(edad >= 18)
+            {
+                Console.WriteLine("La persona es mayor de edad");
+            }
+            else
+            {
+                Console.WriteLine("La persona es menor de edad");
+            }
+        }
+
+    }
+}
